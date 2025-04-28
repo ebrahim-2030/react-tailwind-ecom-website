d6f6fb603cb379b402f3bcde2241196eba0e3ff5
ebrahim <ebrahimasil@gmail.com>
2025-04-28 06:59:43 +0430

    feat: create Offers component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Popular from "../components/Popular";
+import Offers from "../components/Offers";
 
 const Shop = () => {
   return (
     <div className="">
-
       {/* hero section  */}
       <div className="bg-gradient-to-b from-white to-brand/20">
         <section className=" px-3 md:px-6  w-full max-w-[1280px] mx-auto ">
@@ -17,8 +17,14 @@ const Shop = () => {
       <section className="mt-12 md:mt-20 px-3 md:px-6  w-full max-w-[1280px] mx-auto ">
         <Popular />
       </section>
+
+      {/* offers section */}
+      <section className="mt-12 md:mt-20 px-3 md:px-6  w-full max-w-[1024px] mx-auto ">
+        <Offers />
+      </section>
     </div>
   );
 };
 
 export default Shop;
+
