a9716094e782d374270633efd75c9eb5ae40f94e
ebrahim <ebrahimasil@gmail.com>
2025-04-28 08:43:51 +0430

    feat: create Footer component and complete Shop page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { FaInstagram } from "react-icons/fa6";
+import { FaPinterestP, FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="mt-12 md:mt-20 md:px-6 border-t border-light-brand border-opacity-25 flex flex-col items-center pt-10 pb-3 ">
+      <div className="flex items-end gap-2 ">
+        <div className="flex items-end">
+          <img className="h-12 md:h-20 w-12 md:w-20  " src={logo} alt="logo" />
+          <span className="text-xs font-medium text-brand -mb-1">eCom</span>
+        </div>
+        <h2 className="text-2xl font-bold bg-gradient-to-r from-light-brand/50 to-brand bg-clip-text text-transparent uppercase -mb-2">
+          ecommerce
+        </h2>
+      </div>
 
-export default Footer
+      <ul className="flex gap-4 mt-8">
+        <li className="hover:text-brand">
+          <Link>Company</Link>
+        </li>
+
+        <li className="hover:text-brand">
+          <Link>Office</Link>
+        </li>
+        <li className="hover:text-brand">
+          <Link>About</Link>
+        </li>
+        <li className="hover:text-brand">
+          <Link>Contact</Link>
+        </li>
+      </ul>
+      <div className="flex items-center my-6 gap-4">
+        <div className="border p-2">
+          <FaInstagram />
+        </div>
+        <div className="border p-2">
+          <FaPinterestP />
+        </div>
+        <div className="border p-2">
+          <FaWhatsapp />
+        </div>
+      </div>
+      <div className="border-t  pt-2 border-opacity-50">
+        <p> Copyright 2025 - All Right Reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -3,6 +3,7 @@ import offers_img from "../assets/offers.png";
 const Offers = () => {
   return (
     <div className="bg-gradient-to-b from-custom-blue/20 to-white flex flex-col md:flex-row md:justify-center items-center pt-10 pb-5 ">
+      {/* offer text section */}
       <div className="flex flex-col  items-center md:items-start">
         <h2 className="text-3xl md:text-4xl  lg:text-5xl text-custom-blue mb-4 font-bold md:w-[360px] ">
           Exclusive Offers For You
@@ -12,8 +13,14 @@ const Offers = () => {
           Check Now
         </button>
       </div>
+
+      {/* offer image section */}
       <div>
-        <img src={offers_img} className="mt-6 md:mt-0 h-[400px] md:h-[550px] w-[300px] md:w-[400px]" alt="" />
+        <img
+          src={offers_img}
+          className="mt-6 md:mt-0 h-[400px] md:h-[550px] w-[300px] md:w-[400px]"
+          alt=""
+        />
       </div>
     </div>
   );
